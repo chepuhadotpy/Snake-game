d45e0eb6e94b7842d4fd8c24c2f3ac70cbc3c837
SuperPyDroCh <ivashka.pri@ya.ru>
2024-05-17 22:20:07 +0300

    Minor changes

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,5 @@
 from move import bot1, bot2, userkeyboard, show
-from time import sleep
+
 
 print('Выберите режим игры: \n 1: Автоматическая не оптимизированная \n 2: Автоматическая оптимизированная \n 3: С',
       'помощью стрелочек на клавиатуре')
@@ -9,10 +9,8 @@ show()
 if user == 1:
     bot1()
 
-elif user == 2:
+if user == 2:
     bot2()
 
-elif user == 3:
+if user == 3:
     userkeyboard()
-
-sleep(10)
