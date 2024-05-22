ddfbe258d3e54e9e4d308694a7186c0b178a1219
SuperPyDroCh <ivashka.pri@ya.ru>
2024-05-22 13:39:47 +0300

    Minor changes

--- Code/func.py
+++ Code/func.py
@@ -12,12 +12,12 @@ def clear():
 # Функция для создания нового игрового поля:
 def set_a_game_field():
     try:
-        string_numb = random.randint(0, 9)
+        string_numb = random.randint(0, 17)
         before_before_the_apple = string_numb * ('|' + 39 * ' ' + '|' + '\n')
         before_the_apple = random.randrange(0, 40, 2)
         after_the_apple = '#' + (' ' * (38 - before_the_apple) + '|' + '\n')
         before_the_apple = '|' + ' ' * before_the_apple
-        after_after_the_apple = (9 - string_numb) * ('|' + 39 * ' ' + '|' + '\n')
+        after_after_the_apple = (17 - string_numb) * ('|' + 39 * ' ' + '|' + '\n')
         result = (('_' * 41) + '\n') + before_before_the_apple + (before_the_apple + after_the_apple) + after_after_the_apple + (('‾' * 41) + '\n')
         return result
     except Exception as e:
